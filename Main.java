1a5e4122dd12acda50de4fa93e13cfb0f85a9216
adamaxy <adam996@libero.it>
2023-07-31 15:23:23 +0200

    corezione final

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        programmer programmerLunch = new programmer();
+        printProgrammer(programmerLunch);
+
+    }
+    static void printProgrammer( programmer programmer){
+        programmer.name = "Alice";
+        programmer.age = 28;
+        programmer.wearsGlasses = true;
+
+        programmer.drinkCoffee();
+        programmer.printDetails();
+
+        programmer.name = "Bob";
+        programmer.age = 32;
+        programmer.wearsGlasses = false;
+
+        programmer.printDetails();
+        programmer.hasGlasses();
+    }
+}
---
+++ programmer.java
@@ -0,0 +1,23 @@
+public class programmer {
+
+    String name;
+    int age;
+    boolean wearsGlasses;
+
+    void drinkCoffee() {
+        System.out.println("Espresso è il segreto!");
+    }
+
+    void printDetails() {
+        System.out.println(name + " è un programmatore di " + age + " anni");
+    }
+
+    // Metodo per stampare se il programmatore indossa gli occhiali nella console
+    void hasGlasses() {
+        System.out.println("Indossa " + name + " gli occhiali? " + wearsGlasses);
+
+
+
+
+
+    }}
